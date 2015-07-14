2b2f265ca0a51f181ad47280001ee9c7bf616c79
Kevin Renskers <info@mixedcase.nl>
2015-07-14 22:10:04 +0000

    redux-devtools!

--- src/app.js
+++ src/app.js
@@ -2,22 +2,38 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const {Router} = require('react-router');
 const {history} = require('react-router/lib/HashHistory');
-const Redux = require('redux');
+const {createStore, applyMiddleware, combineReducers, compose} = require('redux');
 const {Provider} = require('react-redux');
+const { devTools, persistState } = require('redux-devtools');
+const { DevTools, DebugPanel, LogMonitor } = require('redux-devtools/lib/react');
+const thunk = require('redux-thunk');
 const reducers = require('./reducers');
 const routes = require('./routes');
+require('./styles/main.css');
 
-const reducer = Redux.combineReducers(reducers);
-const store = Redux.createStore(reducer);
+const finalCreateStore = compose(
+  applyMiddleware(thunk),
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+  createStore
+);
+
+const reducer = combineReducers(reducers);
+const store = finalCreateStore(reducer);
 
 class App {
   render() {
     return (
-      <Provider store={store}>
-        {() =>
-          <Router children={routes()} history={history}/>
-        }
-      </Provider>
+      <div>
+        <Provider store={store}>
+          {() =>
+            <Router children={routes()} history={history}/>
+          }
+        </Provider>
+        <DebugPanel top right bottom>
+          <DevTools store={store} monitor={LogMonitor} />
+        </DebugPanel>
+      </div>
     );
   }
 }
